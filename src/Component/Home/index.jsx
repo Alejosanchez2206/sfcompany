ab21b3d8618d8ed08baeb32892ea0e0bc99e4abf
Alejo_sanchez <alejoh908@gmail.com>
2024-05-01 18:35:52 -0500

    add new styles

--- src/Component/Home/index.jsx
+++ src/Component/Home/index.jsx
@@ -18,7 +18,7 @@ function index() {
           de nuestros clientes, sino que también los ayudan a alcanzar nuevos niveles de eficiencia y crecimiento.
         </p>
        
-        <Link to='contact'>Contactactanos</Link>
+        <Link to='contact'>Contáctanos</Link>
       </div>
       <div className='contenido_img'>
         <img className='logo_mangos' src={LogoMangos} alt="mangos_logo" />
