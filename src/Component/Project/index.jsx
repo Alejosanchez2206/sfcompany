7c54cc2b8c66f56336b1dc10dc0d7de975afe115
Alejo_sanchez <alejoh908@gmail.com>
2024-05-13 10:53:31 -0500

    aggregada seccion de proyectos

--- src/Component/Project/index.jsx
+++ src/Component/Project/index.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import './project.css'
+import LogoMangos from '../../assets/mangos.png'
+import project from '../../Component/Project/projec.json'
 function Project() {
   return (
-    <div className='container'>
-      <h1>Project</h1>
+    <div className='containerProyectos'>  
+      <ul class="cards">
+        {project.map((project) => (
+          <li>
+            <a class="card">
+              <img src={project.image} class="card__image" alt="" />
+              <div class="card__overlay">
+                <div class="card__header">
+                  <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>
+                  <img class="card__thumb" src="https://i.imgur.com/avatar.png" alt="" />
+                  <div class="card__header-text">
+                    <h3 class="card__title">{project.name}</h3>
+                  </div>
+                </div>
+                <p class="card__description">{project.description}</p>
+                <button className='btn-project'>View</button>
+              </div>
+            </a>
+          </li>
+        ))}
+
+      </ul>
     </div>
   )
 }
