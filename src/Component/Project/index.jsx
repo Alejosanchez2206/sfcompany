f3496141380ec25208fd63a36ddb8ff65ed76c9f
Alejo_sanchez <alejoh908@gmail.com>
2024-05-19 21:23:30 -0500

    add servicios

--- src/Component/Project/index.jsx
+++ src/Component/Project/index.jsx
@@ -24,7 +24,6 @@ function Project() {
             </a>
           </li>
         ))}
-
       </ul>
     </div>
   )
--- src/Component/Services/index.jsx
+++ src/Component/Services/index.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
 import './servicio.css'
+import project from './projec.json'
 function Service() {
   return (
-    <div className='container'><h1>Services</h1></div>
+    <>
+      <div className='containerservice'>
+        <div className='services'>
+          <h3>Servicios</h3>
+          <h1>Que ofrecemos</h1>
+          <label>El mercado de servicios informáticos en Cartagena es significativo porque no se cuenta con muchas empresas que se especialice en brindar los 3 servicios en una misma compañía que son Mantenimiento, Desarrollo y Redes, especialmente con el crecimiento constante de empresas de diversos sectores que requieren soluciones tecnológicas para mejorar sus operaciones.</label>
+        </div>
+      </div>
+      <div className='containerProyectos'>
+        <ul class="cards">
+          {project.map((project) => (
+            <li>
+              <a class="card">
+                <img src={project.image} class="card__image" alt="" />
+                <div class="card__overlay">
+                  <div class="card__header">
+                    <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>
+                    <img class="card__thumb" src="https://i.imgur.com/avatar.png" alt="" />
+                    <div class="card__header-text">
+                      <h3 class="card__title">{project.name}</h3>
+                    </div>
+                  </div>
+                  <p class="card__description">{project.description}</p>
+                </div>
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   )
 }
 
