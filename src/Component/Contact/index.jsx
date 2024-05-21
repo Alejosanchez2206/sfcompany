334645497cc472523db64aad0b2cb077f669b283
Alejo_sanchez <alejoh908@gmail.com>
2024-05-21 17:59:44 -0500

    add contact

--- src/Component/Contact/index.jsx
+++ src/Component/Contact/index.jsx
@@ -2,9 +2,24 @@ import React from 'react'
 import './contact.css'
 function Home() {
   return (
-    <div className='container'>
-      <h1>Contact</h1>
+    <div className='containercontact'>
+      <h1>Contactanos</h1>
+      <form  className="formcontac" accept-charset="UTF-8" action="https://www.formbackend.com/f/664decaabbf1c319" method="POST">
+        <div className="fieldset">
+          <label htmlFor="name">Name</label>
+          <input type="text" id="name" name="name" required />
+        </div>
+
+        <div className="fieldset">
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" name="email" required />
+        </div>
+
+        <button className='btn-contact' type="submit">Enviar</button>
+      </form>
     </div>
+
+
   )
 }
 
